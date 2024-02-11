688fe6120022679745669a3c2991deb0b167ca28
Abdullah Arham <146736122+thetruespectre@users.noreply.github.com>
2024-02-11 23:47:47 +0600

    Add files via upload

---
+++ missle_two.ino
@@ -0,0 +1,45 @@
+#include <Servo.h>
+#include <Wire.h>
+
+Servo servo1;
+Servo servo2;
+int angle = 0;
+bool active;
+const int addr = 10;
+
+void setup() {
+  // put your setup code here, to run once:
+  servo1.attach(3);
+  servo2.attach(6);
+  servo1.write(90);
+  servo2.write(90);
+  Serial.begin(9600);
+  Wire.begin(addr);
+  Serial.println("started");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Wire.onReceive(fire);
+}
+
+void fire(int f) {
+  Serial.println("FIRING");
+  /*
+  byte a, b;
+  a = Wire.read();
+  b = Wire.read();
+  Serial.println(a);
+  Serial.println(b);
+
+  angle = a;
+  angle = (angle << 8) | b;
+  */
+  angle = Wire.read();
+
+  servo1.write(angle);
+  Serial.println(angle);
+  delay(1000);
+  servo1.write(90);
+  angle = 0;
+}
